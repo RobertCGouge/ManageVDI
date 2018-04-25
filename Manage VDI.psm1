cf7277fb3c2bf8958803d30472ad6d0509d0e43e
Robert Gouge <robert.c.gouge@gmail.com>
2018-04-24 20:10:49 -0500

    Start Update-PublishedDesktop and Unit Tests

--- Manage VDI.psm1
+++ Manage VDI.psm1
@@ -1,5 +1,56 @@
 # Implement your module commands in this script.
 
+function Update-PublishedDesktop {
+    [CmdletBinding()]
+    param (
+        # This is the name or IP of the Delivery Controller to run the command on
+        [Parameter(Mandatory = $true,
+            ParameterSetName = 'Desktop Group', 'Individual Desktop')]
+        [ValidateNotNullOrEmpty]
+        [ValidateScript( {
+                try {
+                    Test-Connection -ComputerName $_ -Quiet -Count 1 -ErrorAction Stop
+                }
+                catch {
+                    throw "Could not connect to '${_}'"
+                }
+            })]
+        [string]
+        $DCC,
+        # This is the Delivery Group you want to update
+        [Parameter(Mandatory = $true,
+            ParameterSetName = 'Desktop Group')]
+        [ValidateNotNullOrEmpty]
+        [string]
+        [ValidateScript( {
+                $true
+            })]
+        $DeliveryGroup,
+        # This is the individual VDI Set you want to update
+        [Parameter(Mandatory = $true,
+        ParameterSetName='Individual Desktop')]
+        [ValidateNotNullOrEmpty]
+        [string]
+        $Desktop,
+        # This is the text that will show in the Citrix StoreFront
+        [Parameter(Mandatory=$true,
+            ParameterSetName = 'Desktop Group', 'Individual Desktop')]
+        [ValidateNotNullOrEmpty]
+        [string]
+        $Text
+
+    )
+
+    begin {
+
+    }
+
+    process {
+    }
+
+    end {
+    }
+}
 
 # Export only the functions using PowerShell standard verb-noun naming.
 # Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
--- test/Manage VDI.Tests.ps1
+++ test/Manage VDI.Tests.ps1
@@ -1,10 +1,28 @@
 $ModuleManifestName = 'Manage VDI.psd1'
 $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
 
+Import-Module '..\Manage VDI.psm1'
+
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
         Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
         $? | Should Be $true
     }
 }
+
+Describe 'Update-PublishedDesktop' {
+    It 'Errors if -DCC is null' {
+        {Update-PublishedDesktop -DCC $null} | Should throw
+    }
+    It 'Errors if -DCC is empty' {
+        {Update-PublishedDesktop -DCC ''} | Should throw
+    }
+    It 'Errors if connection to Delivery Controller via IP fails' {
+        {Update-PublishedDesktop -DCC '192.168.1.20'} | should throw
+    }
+    It 'Errors if connection to Delivery Controller via FQDN fails' {
+        {Update-PublishedDesktop -DCC 'dcc.domain.net'} | should throw
+    }
+}
+
 
